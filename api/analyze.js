d34dd41c10d50e353bfd7cb4a2a8ba90d11e8e1a
Pavel <pavelfantastico@gmail.com>
2024-07-26 00:04:01 +0200

    verb detection without slovnik

--- api/analyze.js
+++ api/analyze.js
@@ -254,7 +254,7 @@ module.exports = async (req, res) => {
             const slovnikData = await fetchSlovnikData(word);
             let partOfSpeechType = determinePartOfSpeechType(slovnikData?.partOfSpeech);
             const priruckaData = await fetchPriruckaData(word, partOfSpeechType);
-            if (!!partOfSpeechType && priruckaData?.table?.['2. osoba']) {
+            if (partOfSpeechType === "NOT_DEFINED" && priruckaData?.table?.['2. osoba']) {
                 partOfSpeechType = 'Sloveso';
             }            
             
